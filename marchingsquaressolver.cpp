44f31cf94a45549068775640ef2fd17ee1269664
Ludovic <ludovic.nicolau@gmail.com>
2021-05-13 15:39:50 +0200

    Add MarchingSquaresSolver

---
+++ marchingsquaressolver.cpp
@@ -0,0 +1,6 @@
+#include "marchingsquaressolver.h"
+
+MarchingSquaresSolver::MarchingSquaresSolver()
+{
+
+}
---
+++ marchingsquaressolver.h
@@ -0,0 +1,11 @@
+#ifndef MARCHINGSQUARESSOLVER_H
+#define MARCHINGSQUARESSOLVER_H
+
+
+class MarchingSquaresSolver
+{
+public:
+    MarchingSquaresSolver();
+};
+
+#endif // MARCHINGSQUARESSOLVER_H
